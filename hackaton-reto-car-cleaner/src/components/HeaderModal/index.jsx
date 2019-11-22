021ab2363a77b5111ab8e09ff46b40291c78db8f
Isela Reyes Perdomo <reyesp.isela@gmail.com>
2019-11-22 11:02:29 -0600

    Modificando estilos para el header y terminando los estilos del confirmation

--- hackaton-reto-car-cleaner/src/components/HeaderModal/index.jsx
+++ hackaton-reto-car-cleaner/src/components/HeaderModal/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { IoMdArrowBack } from "react-icons/io";
 import './style.css';
+
+import logo from "../../images/logowash.png";
 
 const HeaderModal = () => {
     return(
         <header className="header-modal">
-            <span>Flecha</span>
+            <span><IoMdArrowBack className="arrow"/></span>
             <figure>
-                <img src="../images/logowash.png" alt="wash-wash"/>
+                <img src={logo} alt="wash-wash"/>
             </figure>
         </header>
     )
