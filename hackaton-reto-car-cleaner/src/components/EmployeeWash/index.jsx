40f1676f46a7807bfa063d62fb998136d044d767
Isela Reyes Perdomo <reyesp.isela@gmail.com>
2019-11-22 12:12:20 -0600

    Creando componente de información del empleado

---
+++ hackaton-reto-car-cleaner/src/components/EmployeeWash/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const EmployeeWash = () => {
+    return (
+        <div className="employe-wash-box">
+            <figure>
+                <img src="" alt=""/>
+            </figure>
+            
+        </div>
+    )
+}
+
+export default EmployeeWash;
