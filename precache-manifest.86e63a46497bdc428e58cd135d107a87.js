fce089f615562b6f276b79d7255d82db63381e30
Isela Reyes Perdomo <reyesp.isela@gmail.com>
2019-11-21 23:21:11 -0600

    Updates

---
+++ precache-manifest.86e63a46497bdc428e58cd135d107a87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c43786b72d1042df69b0c8b77925160",
+    "url": "./index.html"
+  },
+  {
+    "revision": "39c61ac2ab57df8acafd",
+    "url": "./static/css/main.a904b575.chunk.css"
+  },
+  {
+    "revision": "bc515a562428ea48ab13",
+    "url": "./static/js/2.2ce61e54.chunk.js"
+  },
+  {
+    "revision": "39c61ac2ab57df8acafd",
+    "url": "./static/js/main.4697bd2d.chunk.js"
+  },
+  {
+    "revision": "8f8abe65f7daf660ae1d",
+    "url": "./static/js/runtime-main.02e618b3.js"
+  },
+  {
+    "revision": "c25714544fd4bd3733cdcbf5a078df4e",
+    "url": "./static/media/emile-guillemot-ci7gkM_29wA-unsplash.c2571454.png"
+  }
+]);
