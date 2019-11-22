8b79d630815827c9c9eecc2f1837d05976c7e22a
@sandyGonzalez <sandygonzalez.612@gmail.com>
2019-11-22 12:19:28 -0600

    aceptar

--- hackaton-reto-car-cleaner/src/App.jsx
+++ hackaton-reto-car-cleaner/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import {RegisterEmployee, RegisterUser, Welcome, Home} from './views';
-import ServiceCar from './components/ServiceCar/index'
 import './App.css';
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
         <Route exact path = "/registerUser" render = {props => <RegisterUser/>}></Route>
         <Route exact path="/registerEmployee" render={props => <RegisterEmployee/>}></Route>
         <Route exact path="/home" render={props => <Home/>}></Route>
-        <Route exact path="/serviceCar" render={props => <ServiceCar/>}></Route>
       </Switch>
 
     </Router>
