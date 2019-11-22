3e3be579d2bd440af1e7338fab408c77ea51b97c
@sandyGonzalez <sandygonzalez.612@gmail.com>
2019-11-22 10:52:07 -0600

    guardando cambios

---
+++ hackaton-reto-car-cleaner/src/components/ServiceCar/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import "./style.css";
+
+const ServiceCar = () =>{
+
+    return(
+        <section>
+            <p>Tu coche esta en...</p>
+            <h5>Angus Dr. #2, Delegación Benito Juarez</h5>
+
+            <button className = "buttonServiceClass">Calle</button>
+            <button>Domicilio</button>
+
+            <p>Selecciona tu paquete</p>
+
+            <button>LAVADO EXPRESS</button>
+            <a href="#">Mas info</a>
+
+            <button>LAVADO COMPLETO</button>
+            <a href="#">Mas info</a>
+
+            <button>LAVADO MENSUAL</button>
+            <a href="#">Mas info</a>
+
+
+        </section>
+    )
+
+
+
+
+}
+
+
+
+export default ServiceCar
